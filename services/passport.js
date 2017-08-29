5af4ccff250b006162b18f63ce78200fcec467e9
Raphael Okafor <raphael.okafor89@gmail.com>
2017-08-28 23:30:40 -0400

    add proxy to g strategy

--- services/passport.js
+++ services/passport.js
@@ -23,7 +23,8 @@ passport.use(
     new GoogleStrategy({
         clientID: keys.googleClientID,
         clientSecret: keys.googleClientSecret,
-        callbackURL: '/auth/google/callback'
+        callbackURL: '/auth/google/callback',
+        proxy: true
     }, (accessToken, refreshToken, profile, done) => {
 
         // query the db to make sure this profile id doesn't exist already
